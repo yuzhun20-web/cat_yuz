0ac1ce440a663c1c4bc2a7bd4f78a091741c2b4f
yuzhun20-web <yuzhun20@gmail.com>
2025-09-21 01:45:06 +0800

    Delete sw.js
    Delete

--- sw.js
+++
@@ -1,7 +0,0 @@
-
-self.addEventListener('install',(e)=>{
-  e.waitUntil(caches.open('ronin-v1').then(c=>c.addAll(['./','./index.html','./style.css','./settings.overlay.css','./app.js','./settings.overlay.js','./manifest.webmanifest','./icons/cat.svg'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate',(e)=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
