28d8a9fdef03ae42dcc38cea58fef5093084f335
yuzhun20-web <yuzhun20@gmail.com>
2025-09-21 19:25:02 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,2 @@
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open('csv-ro-v3k-theme2').then(c => c.addAll([
-    './','./index.html','./style.css','./app.js','./manifest.webmanifest','./ronin.overrides.css'
-  ])));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== 'csv-ro-v3k-theme2').map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
+self.addEventListener('install',e=>{e.waitUntil(caches.open('csv-ro-v3k-fix2').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
